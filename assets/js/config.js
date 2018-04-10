e9b0fb56da713320fe77ff352e3500212d19e7a3
Arihant Bengani <arihantb@cloudthat.in>
2018-04-10 19:24:36 +0530

    Initial Commit

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_uaRFnnmma', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'a537ibkur7rdngqqo8c7tqeeu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+		
+    },
+    api: {
+        invokeUrl: 'https://8hjl913gfh.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
---
+++ final_login/final_login/signout.js
@@ -0,0 +1,50 @@
+function checklogin(){
+
+	console.log(window.localStorage.length)
+	if(window.localStorage.length===0){
+		window.location.href='login.html';
+	}
+	else{
+		console.log("Alreay logged in");
+		sessionValid();
+	}
+}
+
+function sessionValid(){
+	if (cognitoUser === undefined){
+		console.log("undefined")
+	}
+	else{
+		cognitoUser.getSession(function(err, session) {
+        if (err) {
+            alert(err);
+            return;
+        }
+    	else if(session.isValid()){
+    		console.log('session validity: ' + session.isValid());
+    		session = window.localStorage;
+    		//window.location.href='cloudbilling.html';
+    	}
+    	else{
+    		console.log('session validity: ' + session.isValid());
+    		window.localStorage.clear();
+    		window.location.href='login.html';
+    	}
+		});
+	}
+}
+
+
+
+function signout(){
+	console.log("Data : "+window.localStorage.getItem('token'))
+    console.log("User Name : "+JSON.stringify(userPool.getCurrentUser()));
+    var cognitoUser = userPool.getCurrentUser()
+    console.log(cognitoUser)
+    window.localStorage.clear();
+    console.log("clear : "+window.localStorage.getItem('token'))
+    cognitoUser.signOut();
+    window.location.href='login.html';
+    console.log("Signout");
+}
+
