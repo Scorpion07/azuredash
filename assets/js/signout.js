eb12edb9c26ae82e538446a27e91c6321e89b67e
RV <rutvikv@cloudthat.in>
2018-04-12 13:21:26 +0530

    updated index.html

--- assets/js/signout.js
+++ assets/js/signout.js
@@ -1,55 +1,56 @@
-function checklogin(){
+function checklogin() {
 
     console.log(window.localStorage.length)
-    if(window.localStorage.length===0){
-        window.location.href='/login.html';
+    if (window.localStorage.length === 0) {
+        window.location.href = '/login.html';
     }
-    else{
+    else {
         console.log("Alreay logged in");
         sessionValid();
     }
 }
 
-function sessionValid(){
-    if (cognitoUser === undefined){
+function sessionValid() {
+    if (cognitoUser === undefined) {
         console.log("undefined")
     }
-    else if(cognitoUser === null){
+    else if (cognitoUser === null) {
         console.log("null")
         signout()
     }
-    else{
-        cognitoUser.getSession(function(err, session) {
+    else {
+        cognitoUser.getSession(function (err, session) {
             if (err) {
                 console.log(err);
                 signout();
                 return;
             }
-            else if(session.isValid()){
+            else if (session.isValid()) {
                 console.log('session validity: ' + session.isValid());
                 session = window.localStorage;
                 //window.location.href='cloudbilling.html';
             }
-            else{
+            else {
                 console.log('session validity: ' + session.isValid());
                 window.localStorage.clear();
-                window.location.href='/login.html';
+                window.location.href = '/login.html';
             }
         });
     }
 }
-function signout(){
-    console.log("Data : "+window.localStorage.getItem('token'))
-    console.log("User Name : "+JSON.stringify(userPool.getCurrentUser()));
-    if(cognitoUser !== null){
+
+function signout() {
+    console.log("Data : " + window.localStorage.getItem('token'))
+    console.log("User Name : " + JSON.stringify(userPool.getCurrentUser()));
+    if (cognitoUser !== null) {
         var cognitoUser = userPool.getCurrentUser()
     }
 
     console.log(cognitoUser)
     window.localStorage.clear();
-    console.log("clear : "+window.localStorage.getItem('token'))
+    console.log("clear : " + window.localStorage.getItem('token'))
     //cognitoUser.signOut();
-    window.location.href='/';
+    window.location.href = '/';
     console.log("Signout");
 }
 
