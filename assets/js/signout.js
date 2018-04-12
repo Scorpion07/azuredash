87297aba1ac325c9cf4902bbbff086b039c2b64d
RV <rutvikv@cloudthat.in>
2018-04-12 13:05:03 +0530

    updated signout.jss

--- assets/js/signout.js
+++ assets/js/signout.js
@@ -1,50 +1,54 @@
 function checklogin(){
 
-	console.log(window.localStorage.length)
-	if(window.localStorage.length===0){
-		window.location.href='login.html';
-	}
-	else{
-		console.log("Alreay logged in");
-		sessionValid();
-	}
+    console.log(window.localStorage.length)
+    if(window.localStorage.length===0){
+        window.location.href='/login.html';
+    }
+    else{
+        console.log("Alreay logged in");
+        sessionValid();
+    }
 }
 
 function sessionValid(){
-	if (cognitoUser === undefined){
-		console.log("undefined")
-	}
-	else{
-		cognitoUser.getSession(function(err, session) {
-        if (err) {
-            alert(err);
-            return;
-        }
-    	else if(session.isValid()){
-    		console.log('session validity: ' + session.isValid());
-    		session = window.localStorage;
-            // console.log(cognitoUser.getSession().getCurrentUser().toString());
-    		//window.location.href='cloudbilling.html';
-    	}
-    	else{
-    		console.log('session validity: ' + session.isValid());
-    		window.localStorage.clear();
-    		window.location.href='login.html';
-    	}
-		});
-	}
+    if (cognitoUser === undefined){
+        console.log("undefined")
+    }
+    else if(cognitoUser === null){
+        console.log("null")
+        signout()
+    }
+    else{
+        cognitoUser.getSession(function(err, session) {
+            if (err) {
+                console.log(err);
+                signout();
+                return;
+            }
+            else if(session.isValid()){
+                console.log('session validity: ' + session.isValid());
+                session = window.localStorage;
+                //window.location.href='cloudbilling.html';
+            }
+            else{
+                console.log('session validity: ' + session.isValid());
+                window.localStorage.clear();
+                window.location.href='/login.html';
+            }
+        });
+    }
 }
-
-
-
 function signout(){
-	console.log("Data : "+window.localStorage.getItem('token'))
+    console.log("Data : "+window.localStorage.getItem('token'))
     console.log("User Name : "+JSON.stringify(userPool.getCurrentUser()));
-    var cognitoUser = userPool.getCurrentUser()
+    if(cognitoUser !== null){
+        var cognitoUser = userPool.getCurrentUser()
+    }
+
     console.log(cognitoUser)
     window.localStorage.clear();
     console.log("clear : "+window.localStorage.getItem('token'))
-    cognitoUser.signOut();
+    //cognitoUser.signOut();
     window.location.href='/';
     console.log("Signout");
 }
