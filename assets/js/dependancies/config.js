dc98684303907a54d84345c94d6c664fb79397d6
Arihant Bengani <arihantb@cloudthat.in>
2018-04-24 14:01:25 +0530

    change config in Master

--- assets/js/dependancies/config.js
+++ assets/js/dependancies/config.js
@@ -2,14 +2,10 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_A4I05HNkU', // e.g. us-east-2_uXboG5pAb // for prod
         userPoolClientId: '68jllcc1osbehc014qspvi2851', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv // for prod
-
-        // userPoolId:'ap-south-1_uaRFnnmma',
-        // userPoolClientId:'a537ibkur7rdngqqo8c7tqeeu',
         region: 'ap-south-1' // e.g. us-east-2
 		
     },
     api: {
         invokeUrl: 'https://resourceapis.cloudthat.com/v1', // for prod
-        // invokeUrlR: '  https://48701zkub7.execute-api.ap-south-1.amazonaws.com/dev' //for dev
     }
 };
