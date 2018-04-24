1fc93daff052df8de595d190d82cd5592ee9a89a
Arihant Bengani <arihantb@cloudthat.in>
2018-04-24 11:56:12 +0530

    added proper filename for export, updated with url for login while creating

--- assets/js/dependancies/config.js
+++ assets/js/dependancies/config.js
@@ -1,12 +1,15 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_A4I05HNkU', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '68jllcc1osbehc014qspvi2851', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_A4I05HNkU', // e.g. us-east-2_uXboG5pAb // for prod
+        userPoolClientId: '68jllcc1osbehc014qspvi2851', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv // for prod
+
+        // userPoolId:'ap-south-1_uaRFnnmma',
+        // userPoolClientId:'a537ibkur7rdngqqo8c7tqeeu',
         region: 'ap-south-1' // e.g. us-east-2
 		
     },
     api: {
-        invokeUrl: 'https://resourceapis.cloudthat.com/v1', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-        invokeUrlR: ' https://gm49v0xuka.execute-api.ap-south-1.amazonaws.com/dev'
+        invokeUrl: 'https://resourceapis.cloudthat.com/v1', // for prod
+        // invokeUrlR: '  https://48701zkub7.execute-api.ap-south-1.amazonaws.com/dev' //for dev
     }
 };
