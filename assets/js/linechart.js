377109ac3c09d4b7cb6aad7503af4f889b07a45d
RV <rutvikv@cloudthat.in>
2018-05-10 16:06:02 +0530

    New Chart Added

--- assets/js/linechart.js
+++ assets/js/linechart.js
@@ -1,5 +1,78 @@
 
 function showLineChart() {
+    var dataset = getDataSet();
+    var month = getMonth();
+    var tick = getTick();
+
+    $.plot($('#line-chart'), dataset, {
+        series: {
+            lines: { show: true },
+            points: {
+                radius: 3,
+                show: true,
+                fill: true
+            },
+        },
+        yaxis : {
+            show: true,
+            tickSize:25,
+            axisLabel: "Cost $",
+            axisLabelUseCanvas: true,
+            axisLabelFontSizePixels: 15,
+            axisLabelPadding: 3
+
+        },
+        xaxis : {
+            ticks:tick,
+            show: true,
+            axisLabel: month.toString(),
+            axisLabelUseCanvas: true,
+            axisLabelFontSizePixels: 15,
+            axisLabelPadding: 10
+        },
+        legend: {
+            noColumns: 0,
+            labelBoxBorderColor: "#000000",
+            position: "nw"
+        },
+        grid: {
+            hoverable: true,
+            borderWidth: 2,
+            borderColor: "#633200",
+            backgroundColor: { colors: ["#ffffff", "#EDF5FF"] }
+        },
+    });
+    //Initialize tooltip on hover
+    function showTooltip(x, y, contents, z) {
+        $('<div id="flot-tooltip">' + contents + '</div>').css({
+            top: y,
+            left: x,
+            "border-color": z,
+        }).appendTo("body").fadeIn(200);
+    }
+
+    $("#line-chart").bind("plothover", function (event, pos, item) {
+        if (item) {
+            if ((previousPoint != item.dataIndex) || (previousLabel != item.series.label)) {
+                previousPoint = item.dataIndex;
+                previousLabel = item.series.label;
+
+                $("#flot-tooltip").remove();
+
+                var x = item.datapoint[0],
+                    y = item.datapoint[1];
+                z = item.series.color;
+                showTooltip(item.pageX-135, item.pageY-30,"<b>" + item.series.label + "</b><br /> Date : " + x + " <br/>Amount : " + y + " $",z);
+            }
+        } else {
+            $("#flot-tooltip").remove();
+            previousPoint = null;
+            previousLabel = null
+        }
+    });
+}
+
+function getDataSet() {
     var currentTime = new Date();
     var date =currentTime.getDate();
     var year = currentTime.getFullYear().toString();
@@ -21,84 +94,86 @@ function showLineChart() {
     var exttrainCostData = []
     var trainCostData = []
 
-    var count = 0;
     for (var i=2;i<date+1;i++){
-        var val;
-        if (i < 10)
+       if (i < 10)
             tempdate = "0" + i.toString();
         else
             tempdate = i.toString();
-        console.log("Inside for loop : "+(count++))
+
         var getUrlDev = 'http://resources.cloudthat.com/' + "cost/dev/" + year + "/" + month + "/" + tempdate + '.json';
         $.getJSON(getUrlDev).then(function (data) {
-            console.log(data.totalAccount)
-            val = parseFloat(data.totalAccount).toFixed(2)
-            console.log("val : "+val)
-            console.log("type val : "+typeof(Number(val)))
-            console.log("Inside json fun : "+(count++))
-            devCostData.push({y:Number(val)})
+            devCostData.push([Number(parseFloat(data.totalAccount).toFixed(2))])
         });
-        // devCostData.push({label:i,y:Number(val)})
-
         var getUrlProd = 'http://resources.cloudthat.com/' + "cost/prod/" + year + "/" + month + "/" + tempdate + '.json';
-        $.getJSON(getUrlProd, function (r) {
+        $.getJSON(getUrlProd).then(function (data) {
 
-            prodCostData.push({y:Number(parseFloat(r.totalAccount).toFixed(2))})
+            prodCostData.push([Number(parseFloat(data.totalAccount).toFixed(2))])
         });
         var getUrlExttrain = 'http://resources.cloudthat.com/' + "cost/exttrain/" + year + "/" + month + "/" + tempdate + '.json';
-        $.getJSON(getUrlExttrain, function (r) {
-            exttrainCostData.push({y:Number(parseFloat(r.totalAccount).toFixed(2))})
+        $.getJSON(getUrlExttrain).then(function (data) {
+            exttrainCostData.push([Number(parseFloat(data.totalAccount).toFixed(2))])
         });
         var getUrlTrain = 'http://resources.cloudthat.com/' + "cost/training/" + year + "/" + month + "/" + tempdate + '.json';
-        $.getJSON(getUrlTrain, function (r) {
-            trainCostData.push({y:Number(parseFloat(r.totalAccount).toFixed(2))})
+        $.getJSON(getUrlTrain).then(function (data) {
+            trainCostData.push([Number(parseFloat(data.totalAccount).toFixed(2))])
         });
     }
 
-
+    for (var i=0;i<date;i++){
+        devCostData[i].splice(0,0,i);
+        prodCostData[i].splice(0,0,i);
+        exttrainCostData[i].splice(0,0,i);
+        trainCostData[i].splice(0,0,i);
+    }
     console.log(devCostData)
     console.log(prodCostData)
     console.log(exttrainCostData)
     console.log(trainCostData)
 
 
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        theme: "light2",
-        title:{
-            text: "Cost Chart"
-        },
-        axisY:{
-            title:"Cost $",
-            includeZero: false
-        },
-        axisX:{
-            title:"Days"
-        },
+    return  [
+        { label: "Developer", data: devCostData, points: { symbol: "triangle",fillColor: "#20629b" }, color: '#20629b'} ,
+        { label: "Production", data: prodCostData, points: { symbol: "square",fillColor: "#ff9d0a" }, color: '#ff9d0a'} ,
+        { label: "External Training", data: exttrainCostData, points: { symbol: "diamond",fillColor: "#ed0b0b" }, color: '#ed0b0b'},
+        { label: "Training", data: trainCostData, points: { symbol: "circle",fillColor: "#73ff08" }, color: '#73ff08'} ,
+    ];
+}
 
-        data: [{
-            type: "line",
-            name: "Developer",
-            color: "#20629b",
-            dataPoints: devCostData
-        },
-            {
-                type: "line",
-                name: "Production",
-                color: "#ff9d0a",
-                dataPoints: prodCostData
-            },{
-                type: "line",
-                name: "External Training",
-                color: "#fffa09",
-                dataPoints: exttrainCostData
-            },{
-                type: "line",
-                name: "Training",
-                color: "#73ff08",
-                dataPoints: trainCostData
-            }]
-    });
-    chart.render();
+function getMonth() {
+    var currentTime = new Date();
+    var month = currentTime.getMonth();
+    if(month === 0)
+        return "January";
+    else if(month === 1)
+        return "February"
+    else if(month === 2)
+        return "March"
+    else if(month === 3)
+        return "April"
+    else if(month === 4)
+        return "May"
+    else if(month === 5)
+        return "June"
+    else if(month === 6)
+        return "july"
+    else if(month === 7)
+        return "August"
+    else if(month === 8)
+        return "September"
+    else if(month === 9)
+        return "octomber"
+    else if(month === 10)
+        return "Novemnber"
+    else
+        return "December"
+}
 
+function getTick() {
+    var currentTime = new Date();
+    var date =currentTime.getDate();
+    var tick = []
+    for (var i=0;i<date;i++){
+        tick.push([i,(i+1).toString()])
+    }
+    return tick;
 }
