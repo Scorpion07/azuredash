0f3c292b4d8b317640819bbc357eeb98b91fd0d4
Arihant Bengani <arihantb@cloudthat.in>
2018-05-14 13:29:42 +0530

    updated to CF to master for

--- assets/js/dependancies/config.js
+++ assets/js/dependancies/config.js
@@ -1,12 +1,15 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_uaRFnnmma',
-        userPoolClientId: 'a537ibkur7rdngqqo8c7tqeeu',
+        userPoolId: 'ap-south-1_A4I05HNkU', // e.g. us-east-2_uXboG5pAb // for prod
+        userPoolClientId: '68jllcc1osbehc014qspvi2851', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv // for prod
+
+        // userPoolId: 'ap-south-1_uaRFnnmma',
+        // userPoolClientId: 'a537ibkur7rdngqqo8c7tqeeu',
         region: 'ap-south-1'
+
     },
     api: {
-
-        invokeUrl: 'https://8hjl913gfh.execute-api.ap-south-1.amazonaws.com/dev',
-        invokeUrlR: 'https://gm49v0xuka.execute-api.ap-south-1.amazonaws.com/dev'
+        invokeUrl: 'https://resourceapis.cloudthat.com/v1', // for prod
+        // invokeUrlR: '  https://48701zkub7.execute-api.ap-south-1.amazonaws.com/dev' //for dev
     }
 };
