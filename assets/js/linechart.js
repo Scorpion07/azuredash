69d857d95e42052a43429e7586be6ceb5b498b9b
prarthitm <prarthitm@cloudthat.in>
2018-05-17 17:52:39 +0530

    Changed Color Pattle to CloudThat Colors in Chart

--- assets/js/linechart.js
+++ assets/js/linechart.js
@@ -56,7 +56,7 @@ function showLineChart() {
         legend: {
             // noColumns: 4,
             labelBoxBorderColor: "#000000",
-            position: "nw",
+            position: "nw"
             // margin: "2%"
         },
         grid: {
